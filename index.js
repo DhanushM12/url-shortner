a04a91efdc22d1f092cf5548dacd335f0ac46838
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-26 20:37:30 +0530

    added mongoose config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+const db  = require('./config/mongoose');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {

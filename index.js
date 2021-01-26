359b7a790eee22a90d602240e304a88c5272c8dd
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-26 20:49:12 +0530

    added the schema

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const db  = require('./config/mongoose');
+const ShortUrl  = require('./models/shortUrl');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', {shortUrls: shortUrls});
+})
+
+app.post('/shortUrls', async (re1, res) => {
+    await ShortUrl.create({full : req.body.fullURL});
+
+    res.redirect('/');
 })
 app.listen(port, function(err){
     if(err){

7f868a4205f756a4bf4affba62c57788512237a9
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-26 20:30:42 +0530

    added the views

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running server ${err}`);
